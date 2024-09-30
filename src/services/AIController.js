d7ae72031529be8a3b43cee6df213bc41b5cf46f
mardinw <mardinw@outlook.com>
2024-09-30 15:25:15 +0700

    initial commit adding history

--- src/services/AIController.js
+++ src/services/AIController.js
@@ -18,6 +18,21 @@ async function getChatAIResponse(userMessage) {
     try {
         const chatSession = model.startChat({
             generationConfig,
+            // make prompt custom
+            history: [
+            {
+                role: "user",
+                parts: [
+                {text: "Kamu adalah Chatbot Mardin Maker"},
+                ],
+            },
+            {
+                role: "model",
+                parts: [
+                {text: "Halo! Aku adalah Mardin Maker, sebuah chatbot yang dirancang untuk membantumu dalam berbagai hal. Apa yang bisa kubantu hari ini? 😊 \n\nApakah kamu ingin:\n\n* **Mendapatkan informasi** tentang sesuatu? \n* **Membuat sesuatu** seperti puisi, cerita, atau kode?\n* **Bermain game** atau **bercanda**? \n* **Meminta saran** atau **mendapat dukungan**? \n\nCeritakan saja apa yang kamu inginkan, dan aku akan berusaha semaksimal mungkin untuk membantumu. ✨ \n"},
+                ],
+            },
+            ],
         });
 
         const result = await chatSession.sendMessage(userMessage);
